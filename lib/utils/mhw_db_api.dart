95b71f9b332c17e2dd57cd8269d660eca643957b
JohnAstro <jonathan.gonzalez12@utrgv.edu>
2022-06-07 05:59:43 -0500

    fixing api class

--- lib/utils/mhw_db_api.dart
+++ lib/utils/mhw_db_api.dart
@@ -1,19 +1,21 @@
+import 'package:flutter_mhwdb_app/utils/ailment.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class MhwAPI {
-  static Future<List<dynamic>> fetchAilments(http.Client client) async {
+  static Future<List<Ailment>> fetchAilments(http.Client client) async {
     var url = Uri.parse('https://mhw-db.com/ailments');
 
     // Get response from client given url
     final response = await client.get(url);
 
     // Decode jason response from api to a Map<String, dynamic> variable
-    List<dynamic> json = jsonDecode(response.body);
-
+    var json = jsonDecode(response.body) as List;
+    List<Ailment> names =
+        json.map((namejson) => Ailment.fromJson(namejson)).toList();
     // If HTTP status code 200 (i.e., successful) return the ailments\
     if (response.statusCode == 200) {
-      return json;
+      return names;
     } else {
       throw Exception('Failed to load Ailments');
     }
