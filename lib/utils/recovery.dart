096615bc81357abf898dd88390a0c8bcfd86e8b8
JohnAstro <jonathan.gonzalez12@utrgv.edu>
2022-06-10 23:33:29 -0500

    fix factory

--- lib/utils/recovery.dart
+++ lib/utils/recovery.dart
@@ -1,17 +1,28 @@
-class Recovery {
-  List<dynamic> actions;
-  // List<Item> items;
+import 'package:flutter_mhwdb_app/utils/item.dart';
 
-  Recovery({
-    this.actions,
-  });
+class Recovery {
+  final List<dynamic> actions;
+  final List<Item> items;
+
+  Recovery({this.actions, this.items});
 
   factory Recovery.fromJson(dynamic json) {
-    if (json["actions"] != null) {
+    if (json["actions"] != null && json["items"] != null) {
       var jsonActions = json["actions"] as List;
-      return Recovery(actions: jsonActions);
+      var jsonItems = json["items"] as List;
+      List<Item> _items =
+          jsonItems.map((itemInJson) => Item.fromJson(itemInJson)).toList();
+      return Recovery(actions: jsonActions, items: _items);
+    } else if (json["actions"] != null) {
+      var jsonActions = json["actions"] as List;
+      return Recovery(actions: jsonActions, items: null);
+    } else if (json["items " != null]) {
+      var jsonItems = json["items"] as List;
+      List<Item> _items =
+          jsonItems.map((itemInJson) => Item.fromJson(itemInJson)).toList();
+      return Recovery(actions: null, items: _items);
     } else {
-      return Recovery(actions: null);
+      return Recovery(actions: null, items: null);
     }
   }
 }
